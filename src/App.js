36c6741b951b4c4b7ff262d8c2c33ccdcab2a9bf
Weriks Santos <you@example.comwerikscs@gmail.com>
2022-04-01 12:17:44 -0300

    create: adds component InitialScreen in App component

--- src/App.js
+++ src/App.js
@@ -1,9 +1,10 @@
 import './App.css';
+import InitialScreen from './components/InitialScreen';
 
 function App() {
   return (
     <div className="App">
-      
+      <InitialScreen/>
     </div>
   );
 }
