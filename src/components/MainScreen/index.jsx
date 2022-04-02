7c65dfc6113d67596edd7e303596401dce74d6fa
Weriks Santos <you@example.comwerikscs@gmail.com>
2022-04-02 18:39:53 -0300

    feat: adds to form js funcionalities

--- src/components/MainScreen/index.jsx
+++ src/components/MainScreen/index.jsx
@@ -6,8 +6,15 @@ import List from '../List';
 
 import './styles.css';
 
+import { useState } from 'react';
+
 const MainScreen = ({setIsOnInitialScreen}) => {
 
+  const [listTransactions, setListTransactions] = useState([
+    { description: "Salário recebido", type: "Entrada", value: 2500 },
+    { description: "Conta de luz", type: "Despesa", value: -150 }
+  ]);
+  
   const backToInitialScreen = () => {
     setIsOnInitialScreen(true)
   }
@@ -17,12 +24,15 @@ const MainScreen = ({setIsOnInitialScreen}) => {
       <MainHeader backToInitialScreen={backToInitialScreen}/>
       <main className='main'>
         <section className='main__form-total-money'>
-          <Form/>
+          <Form
+            listTransactions={listTransactions}
+            setListTransactions={setListTransactions}
+          />
           <TotalMoney/>
         </section>
         <section className='main__filters-list'>
           <Filters/>
-          <List/>
+          <List listTransactions={listTransactions}/>
         </section>
       </main>
     </>
