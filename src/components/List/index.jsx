3b3ce21229cc779f32bc4586c3ec20791bf81aed
Weriks Santos <you@example.comwerikscs@gmail.com>
2022-04-02 18:55:05 -0300

    feat: implements feature to add card dinamically

--- src/components/List/index.jsx
+++ src/components/List/index.jsx
@@ -2,10 +2,23 @@ import Card from '../Card';
 import CardEmpty from '../CardEmpty';
 import './styles.css';
 
-const List = () => {
+const List = ({listTransactions}) => {
+
+  const cardList = listTransactions.map(({description, type, value}, i) => {
+    return (
+      <Card
+        key={i}
+        description={description}
+        cost={value}
+        type={type}
+      />
+    )
+  });
+
   return (
     <ul className="card-list">
-      <CardEmpty/>
+      {cardList}
+      {/* <CardEmpty/>
       <Card
         description='Salário - Mês Dezembro'
         cost='6.660,00'
@@ -45,7 +58,7 @@ const List = () => {
         description='Salário - Mês Dezembro'
         cost='6.660,00'
         type='Entrada'
-      />
+      /> */}
       {/* <Card/> */}
     </ul>
   )
